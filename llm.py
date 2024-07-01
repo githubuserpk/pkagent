fdd3ddb8e9394eb6298d894a05852b959b309c41
githubuserpk <pradip.tk@gmail.com>
2024-07-01 13:07:34 +0100

    -

--- llm.py
+++ llm.py
@@ -8,9 +8,17 @@ print(f"openai api key: {openai_key}")
 
 llm = ChatOpenAI(api_key=openai_key, model: str = "gpt-3.5-turbo")
 
+
+
 response = llm.invoke("Hello, how are you")
 print(response)
 
 
+chat = ChatOpenAI(
+    model_name='gpt-3.5-turbo-16k',
+    temperature = self.config.llm.temperature,
+    openai_api_key = self.config.llm.openai_api_key,         
+    max_tokens=self.config.llm.max_tokens
+)
 
 
